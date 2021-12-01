f43804968c4165a7e19d884e32f1e243887dc458
Tom Praschan <13141438+tom-anders@users.noreply.github.com>
2021-12-01 08:35:00 -0800

    Tighten up the solution a bit

--- days/day1/src/lib.rs
+++ days/day1/src/lib.rs
@@ -1,24 +1,12 @@
-fn count_increase(v: &Vec<i64>) -> usize {
+fn count_increase(v: &Vec<i64>, skip: usize) -> usize {
     v.iter()
-        .zip(v.iter().skip(1))
-        .map(|(lhs, rhs)| lhs - rhs)
-        .filter(|d| d < &0)
+        .zip(v.iter().skip(skip))
+        .filter(|(lhs, rhs)| rhs > lhs)
         .count()
 }
 
 pub fn solve(input_file: &str) -> utils::Solution {
-    let report: Vec<i64> = std::fs::read_to_string(input_file)
-        .unwrap()
-        .lines()
-        .map(|s| s.parse().unwrap())
-        .collect();
+    let report = utils::get_lines_as(input_file);
 
-    let part2: Vec<i64> = report
-        .iter()
-        .zip(report.iter().skip(1))
-        .zip(report.iter().skip(2))
-        .map(|((x, y), z)| x + y + z)
-        .collect();
-
-    utils::Solution::new(count_increase(&report), count_increase(&part2))
+    utils::Solution::new(count_increase(&report, 1), count_increase(&report, 3))
 }
