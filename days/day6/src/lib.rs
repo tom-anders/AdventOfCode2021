1b30cb974257f26f0c9ee21c1b2cb4b75296ea9b
tom-anders <13141438+tom-anders@users.noreply.github.com>
2021-12-05 23:34:31 -0800

    Use Erik's elegant approach for day 6

--- days/day6/src/lib.rs
+++ days/day6/src/lib.rs
@@ -3,30 +3,22 @@ use std::collections::HashMap;
 
 use utils::{Solution, Input};
 
-fn simulate(fish: Vec<i64>, days: i64) -> usize {
-    let mut cycles = vec![0, 0, 0, 0, 0, 0, 0];
+fn simulate(fish: Vec<i64>, days: i64) -> i64 {
+    let mut ages: Vec<i64> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
     for f in &fish {
-        cycles[*f as usize] += 1;
+        ages[*f as usize] += 1;
     }
 
-    let mut skip = vec![0, 0, 0, 0, 0, 0, 0];
-    for i in 0..days {
-        let today_cycle = (i % 7) as usize;
-
-        let num_new_offspring = cycles[today_cycle] - skip[today_cycle];
-        skip[today_cycle] = 0;
-
-        let new_cycle = ((i + 2) % 7) as usize;
-        cycles[new_cycle] += num_new_offspring;
-
-        skip[new_cycle] = num_new_offspring;
+    for _ in 0..days {
+        ages.rotate_left(1);
+        ages[6] += *ages.last().unwrap();
     }
 
-    cycles.iter().sum()
-
+    ages.iter().sum()
 }
 
 pub fn solve(input: Input) -> Solution {
     let fish: Vec<i64> = input.raw.split(',').map(|i| i.trim().parse().unwrap()).collect();
     Solution::new(simulate(fish.clone(), 80), simulate(fish.clone(), 256))
+    // Solution::new(simulate(fish.clone(), 18), "")
 }
